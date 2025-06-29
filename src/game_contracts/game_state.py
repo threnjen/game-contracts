b5661496e4db04f7a17ecad3804c7f31cc76bfe1
threnjen <threnjen@gmail.com>
2025-06-29 11:06:06 -0700

    add command to gamestate base model

--- src/game_contracts/game_state.py
+++ src/game_contracts/game_state.py
@@ -1,7 +1,17 @@
 from pydantic import BaseModel
+from abc import abstractmethod
 
 
 class GameState(BaseModel):
     players: dict
     targetables: dict
     cards: dict
+
+
+class Command(BaseModel):
+    source: str
+    target: str
+
+    @abstractmethod
+    def execute(self, state):
+        pass
